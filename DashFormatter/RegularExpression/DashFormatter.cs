90bc38251e99ba7b4f36f48b3f260fbdabb8d27d
abdo samara <abdoo.ahmed38@icloud.com>
2022-06-30 14:14:13 +0200

    Initial Commit

---
+++ DashFormatter/RegularExpression/DashFormatter.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class DashFormatter
+    {
+        public enum IndentStyleValue
+        {
+            Space,
+            Tab
+        }
+
+        private List<int> closeTabs = new List<int>();
+
+        public DashFormatter(IndentStyleValue indentStyle, int indentCount)
+        {
+            this.IndentStyle = indentStyle;
+            this.IndentCount = indentCount;
+        }
+
+        public IndentStyleValue IndentStyle { get; private set; }
+        public int IndentCount { get; private set; }
+        
+        public string Process(string value)
+        {
+            this.ValidateBraces(value);
+            var matched = Regex.Replace(value, @":|,", m => string.Format(@"{0}" + Environment.NewLine, m.Value));
+            matched = Regex.Replace(matched, @"#{|={|}", m => string.Format(Environment.NewLine + @"{0}" + Environment.NewLine, m.Value));
+            matched = Regex.Replace(matched, @"" + Environment.NewLine + Environment.NewLine, Environment.NewLine);
+            string[] array = matched.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            this.closeTabs.Clear();
+            var result =  this.ProcessInternal(array, 0, 0, 0);
+            return string.Join(Environment.NewLine, result);
+        }
+
+        private string[] ProcessInternal(string[] data, int tab, int index, int close)
+        {
+            if (index == data.Length)
+            {
+                return data;
+            }
+            var tabs = "";
+            if (index == 0)
+            {
+                this.ProcessInternal(data, tab, index + 1, close);
+            }
+            if (Regex.IsMatch(data[index], @":"))
+            {
+                try
+                {
+                    if (Regex.IsMatch(data[index - 1], @"{"))
+                    {
+                        tab = tab + IndentCount;
+                    }
+                }
+                catch (Exception e)
+                {
+                    tab = tab;
+                }
+
+                for (var x = 1; x <= tab; x++)
+                {
+                    tabs += " ";
+                }
+                data[index] = tabs + data[index];
+                this.ProcessInternal(data, tab, index + 1, close);
+                return data;
+            }
+            else
+            {
+                if (Regex.IsMatch(data[index - 1], @","))
+                {
+
+                    for (var x = 1; x <= tab; x++)
+                    {
+                        tabs += " ";
+                    }
+                    data[index] = tabs + data[index];
+                    this.ProcessInternal(data, tab, index + 1, close);
+                    return data;
+                }
+                else if (Regex.IsMatch(data[index], @","))
+                {
+                    tab = tab - IndentCount;
+                    for (var x = 1; x <= tab; x++)
+                    {
+                        tabs += " ";
+                    }
+                    data[index] = tabs + data[index];
+                    this.ProcessInternal(data, tab, index + 1, close);
+                    return data;
+                }
+                if (Regex.IsMatch(data[index - 1], @":|,"))
+                {
+                    if (!Regex.IsMatch(data[index + 1], @"{"))
+                    {
+                        for (var x = 1; x <= tab; x++)
+                        {
+                            tabs += " ";
+                        }
+                        data[index] = tabs + data[index];
+                        this.ProcessInternal(data, tab, index + 1, close);
+                        return data;
+                    }
+                    else
+                    {
+                        if (Regex.IsMatch(data[index], @"{"))
+                        {
+                            this.closeTabs.Add(tab);
+                            close = close + 1;
+                        }
+                        else
+                        {
+                            tab = tab + IndentCount;
+                        }
+                        for (var x = 1; x <= tab; x++)
+                        {
+                            tabs += " ";
+                        }
+                        var value = tabs + data[index];
+                        data[index] = tabs + data[index];
+                        this.ProcessInternal(data, tab, index + 1, close);
+                    }
+
+                }
+                else
+                {
+                    if (Regex.IsMatch(data[index], @"{"))
+                    {
+                        if (!Regex.IsMatch(data[index - 1], @"}"))
+                        {
+                            tab = tab + IndentCount;
+                        }
+                        this.closeTabs.Add(tab);
+                        close = close + 1;
+
+                    }
+                    else if (Regex.IsMatch(data[index], @"}"))
+                    {
+                        tab = this.closeTabs[close - 1];
+                        this.closeTabs.RemoveAt(close - 1);
+                        close = close - 1;
+                    }
+                    else
+                    {
+                        tab = tab + IndentCount;
+                    }
+                    for (var x = 1; x <= tab; x++)
+                    {
+                        tabs += " ";
+                    }
+                    var value = tabs + data[index];
+                    data[index] = tabs + data[index];
+                    this.ProcessInternal(data, tab, index + 1, close);
+                }
+            }
+            return data;
+        }
+
+
+        private void ValidateBraces(string data)
+        {
+            var open = Regex.Matches(data, @"{");
+            var close = Regex.Matches(data, @"}");
+            if (open.Count != close.Count)
+            {
+                throw new ArgumentException("the count of open not equal close");
+            }
+            
+        }
+    }
+}
---
+++ DashFormatter/RegularExpression/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegularExpression
+{
+    public partial class Form1 : Form
+    {
+        static string[] result;
+        static List<int> Closetabs = new List<int>();
+        static int spaces = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                spaces = 1;
+            }
+            else
+            {
+                spaces = Convert.ToInt32(textBox2.Text);
+            }
+            var Formatter = new DashFormatter(DashFormatter.IndentStyleValue.Space, spaces);
+            try
+            {
+                textBox1.Text = Formatter.Process(TextBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("the count of open not equal close");
+            }
+            
+        }
+
+        static bool CheckStringValidation(string data)
+        {
+            var open = Regex.Matches(data, @"{");
+            var close = Regex.Matches(data, @"}");
+            if(open.Count == close.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var data = TextBox.Text;
+            var array = data.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            for (var a = 0; a < array.Length; a++)
+            {
+                array[a] = array[a].Trim();
+            }
+            data = string.Join("", array);
+            textBox1.Text = data;
+        }
+    }
+}
